172129f0de5afc38ff36bff8e23367d73fcf7ab3
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-11-06 12:31:19 +0100

    brute-force method to find shorter functions

---
+++ py/find.py
@@ -0,0 +1,232 @@
+import sys
+from os.path import dirname, join, realpath
+from lib.ast import *
+from itertools import chain
+
+def arguments():
+    yield (Variable('A'), Variable('B'))
+    yield (Variable('A'), Variable('C'))
+    yield (Variable('B'), Variable('C'))
+    yield (Variable('A'), Negation(Variable('B')))
+    yield (Variable('A'), Negation(Variable('C')))
+    yield (Variable('B'), Negation(Variable('C')))
+    yield (Negation(Variable('A')), Variable('B'))
+    yield (Negation(Variable('A')), Variable('C'))
+    yield (Negation(Variable('B')), Variable('C'))
+    yield (Variable('A'), Constant(True))
+    yield (Variable('B'), Constant(True))
+    yield (Variable('C'), Constant(True))
+
+def ops():
+    yield 'and'
+    yield 'or'
+    yield 'xor'
+
+def two_ops():
+    for op1 in ops():
+        for op2 in ops():
+            yield (op1, op2)
+
+def three_ops():
+    for op1 in ops():
+        for op2 in ops():
+            for op3 in ops():
+                yield (op1, op2, op3)
+
+def three_pairs_of_args():
+    for arg1 in arguments():
+        for arg2 in arguments():
+            for arg3 in arguments():
+                yield (arg1, arg2, arg3)
+
+def two_pairs_of_args():
+    for arg1 in arguments():
+        for arg2 in arguments():
+            yield (arg1, arg2)
+
+
+def functions_3levels():
+    for level0 in ops():
+        for level1 in two_ops():
+            for level2 in three_ops():
+                for args in three_pairs_of_args():
+                    op2_0 = Binary(level2[0], args[0][0], args[0][1])
+                    op2_1 = Binary(level2[1], args[1][0], args[1][1])
+                    op2_2 = Binary(level2[2], args[2][0], args[2][1])
+
+                    op1_0 = Binary(level1[0], op2_0, op2_1)
+                    op1_1 = Binary(level1[1], op2_1, op2_2)
+
+                    op_0 = Binary(level0, op1_0, op1_1)
+
+                    yield op_0
+
+
+def functions_2levels():
+    for level0 in ops():
+        for level1 in two_ops():
+            for args in three_pairs_of_args():
+                op1_0 = Binary(level1[0], args[0][0], args[0][1])
+                op1_1 = Binary(level1[1], args[1][0], args[1][1])
+
+                op_0 = Binary(level0, op1_0, op1_1)
+
+                yield op_0
+
+
+def functions_1level():
+    for op in ops():
+        for arg0, arg1 in arguments():
+            yield Binary(op, arg0, arg1)
+
+
+from lib.bodygen import BodyGenerator
+from lib.load import load
+from lib.simplify import simplify
+
+
+Target_SSE      = 10
+Target_AVX2     = 20
+Target_XOP      = 30
+
+def parse_args(args):
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    parser.add_option(
+        "--target",
+        help="choose target (SSE, AVX2, XOP)"
+    )
+
+    parser.add_option(
+        "-o",
+        dest="filename",
+        help="output file"
+    )
+
+    (options, rest) = parser.parse_args(args)
+    if options.filename is None:
+        parser.error("-o is required")
+
+    if options.target is None:
+        parser.error("--target is required")
+
+    if options.target.lower() == 'sse':
+        options.target = Target_SSE
+    elif options.target.lower() == 'avx2':
+        options.target = Target_AVX2
+    elif options.target.lower() == 'xop':
+        options.target = Target_XOP
+    else:
+        valid = ('sse', 'avx2', 'xop')
+        parser.error("--target expects: %s" % ', '.join(valid))
+
+    return options
+
+
+def get_file(name, mode='rt'):
+    dir = dirname(realpath(__file__))
+
+    return open(join(dir, name), mode)
+
+
+class Application:
+    def __init__(self, options, functions):
+        self.options   = options
+        self.functions = functions
+
+        if self.options.target == Target_SSE:
+            from lib.lowering_sse   import transform
+            from lib.assembler_sse  import AssemblerSSE as AssemblerClass
+            self.transform = transform
+            self.AssemblerClass = AssemblerClass
+        elif self.options.target == Target_AVX2:
+            from lib.lowering_sse   import transform
+            from lib.assembler_avx2 import AssemblerAVX2 as AssemblerClass
+            self.transform = transform
+            self.AssemblerClass = AssemblerClass
+        elif self.options.target == Target_XOP:
+            from lib.lowering_xop   import transform
+            from lib.assembler_xop  import AssemblerXOP as AssemblerClass
+            self.transform = transform
+            self.AssemblerClass = AssemblerClass
+
+
+        self.improved = set()
+
+
+    def run(self):
+        self.load()
+        self.find_better()
+        self.save()
+
+
+    def pick_best(self, path):
+        with get_file(path, 'rt') as f:
+            tmp = load(f)
+            for index, (string, expr) in tmp.iteritems():
+                root = self.transform(expr)
+                gen  = BodyGenerator(root, self.AssemblerClass())
+                body = gen.run()
+                if index not in self.data:
+                   self.data[index] = (root, body)
+                elif len(body) < len(self.data[index]):
+                   self.data[index] = (root, body)
+
+
+    def load(self):
+        self.data = {}
+        paths = ('data/intel.txt', 'data/manually_optimized.txt')
+        for path in paths:
+            self.pick_best(path)
+
+
+    def find_better(self):
+        processed = set()
+        for fun in self.functions:
+
+            simpl = simplify(fun)
+            r     = repr(simpl)
+            if r in processed:
+                continue
+
+            processed.add(r)
+
+            root  = self.transform(simpl)
+            gen   = BodyGenerator(root, self.AssemblerClass())
+            body  = gen.run()
+            index = fun.bin()
+            if len(body) < len(self.data[index][1]):
+                print
+                print "improved %02x (%s):" % (index, root)
+                print "was:"
+                print "\n".join(self.data[index][1])
+                print "better:"
+                print "\n".join(body)
+                print
+                self.data[index] = (root, body)
+                self.improved.add(index)
+
+
+    def save(self):
+        if not self.improved:
+            return
+
+        with open(self.options.filename, 'wt') as file:
+            for index in sorted(self.improved):
+                expr = self.data[index][0]
+                file.write('# %s\n' % expr)
+                file.write('%02x %r\n' % (index, expr))
+
+
+def main():
+    options     = parse_args(sys.argv)
+    functions   = chain(functions_1level(), functions_2levels(), functions_3levels())
+    app = Application(options, functions)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ py/lib/simplify.py
@@ -0,0 +1,92 @@
+from ast import *
+
+def simplify(root):
+
+    if isinstance(root, (Constant, Variable)):
+        return root
+
+    if isinstance(root, Negation):
+        v = simplify(root.value)
+        if isinstance(v, Constant):
+            return Constant(not v.value)
+
+        if isinstance(v, Negation):
+            return v.value # not (not x) = x
+
+        return Negation(v)
+
+    if isinstance(root, Binary):
+        a = simplify(root.a)
+        b = simplify(root.b)
+        if root.op in set(('or', 'and', 'xor', 'notand')):
+            if isinstance(a, Variable) and isinstance(b, Variable) and a.var == b.var:
+                
+                if root.op in ('or', 'and'):
+                    return a # x or x = x, also x and x = x
+                elif root.op in ('xor', 'notand'):
+                    return Constant(False) # x xor x = 0
+
+            if isinstance(a, Constant) and isinstance(b, Constant):
+                if root.op == 'or':
+                    return Constant(a.value or b.value)
+                elif root.op == 'and':
+                    return Constant(a.value and b.value)
+                elif root.op == 'xor':
+                    return Constant((a.value and not b.value) or (not a.value and b.value))
+            
+            if isinstance(a, Constant):
+                if root.op == 'or':
+                    if a.value:
+                        return Constant(True)   # x or 1 = 1
+                    else:
+                        return b                # x or 0 = x
+
+                if root.op == 'and':
+                    if a.value:
+                        return b                # x and 1 = x
+                    else:
+                        return Constant(False)  # x and 0 = 0
+
+                if root.op == 'xor':
+                    if a.value:
+                        return Negation(b)      # x xor 1 = not x
+                    else:
+                        return b                # x and 0 = x
+
+            if isinstance(b, Constant):
+
+                if root.op == 'or':
+                    if b.value:
+                        return Constant(True)
+                    else:
+                        return a
+
+                if root.op == 'and':
+                    if b.value:
+                        return a
+                    else:
+                        return Constant(False)
+
+                if root.op == 'xor':
+                    if b.value:
+                        return Negation(a)
+                    else:
+                        return a
+
+            return Binary(root.op, a, b)
+
+    if isinstance(root, Condition):
+
+        var   = simplify(root.var)
+        true  = simplify(root.true)
+        false = simplify(root.false)
+
+        if isinstance(var, Constant):
+            if var.value:
+                return true
+            else:
+                return false
+
+        return Condition(var, true, false)
+
+    assert False, root
